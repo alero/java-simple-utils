12d84c038594d9b71f89542ef4f7f9460e0ec2b0
robert.alexandersson@gmail.com <robert.alexandersson@gmail.com@e272d091-a4ff-c837-5678-7f0eb064b8ca>
2010-04-11 19:29:44 +0000

    Added tdd utility for dateutil

---
+++ simple-java-utils/core/src/main/java/org/hrodberaht/directus/tdd/TestUtilDateUtil.java
@@ -0,0 +1,15 @@
+package org.hrodberaht.directus.tdd;
+
+/**
+ * Simple Java Utils
+ *
+ * @author Robert Alexandersson
+ *         2010-apr-11 21:22:22
+ * @version 1.0
+ * @since 1.0
+ */
+public class TestUtilDateUtil {
+    public static void setNowDate(){
+        
+    }
+}
